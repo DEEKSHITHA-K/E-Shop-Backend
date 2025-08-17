ed5526aaa92923223677c21ef77dd2fb8991a5ca
Deekshitha Kashetty <87613821+DEEKSHITHA-K@users.noreply.github.com>
2025-08-17 23:26:08 +0530

    Update WebConfig.java

--- src/main/java/com/example/backend/config/WebConfig.java
+++ src/main/java/com/example/backend/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000", "https://e-shop-frontendapp.netlify.app")
+                        .allowedOrigins("https://e-shop-frontendapp.netlify.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
             }
         };
